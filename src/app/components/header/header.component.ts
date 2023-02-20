3cd87c21dd5a887debb5e0aae5e083bb8b56f72c
Guillaume Keerhem <guillaumekeerhem@MacBook-Pro-de-Guillaume.local>
2023-02-20 10:05:48 +0100

    Created header and mock data for pokemons

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+import * as content from '../../../data/content.json';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css'],
+})
+export class HeaderComponent {
+  content = content;
+}
